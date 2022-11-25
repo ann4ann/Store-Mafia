00b731eb203d3c0366938feecd6a2b3a5daccc0f
Nuraly Sharapiyev <r.sharapiev@gmail.com>
2022-11-25 11:24:19 +0300

    added card component

---
+++ src/app/components/card/card.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./card.css";
+
+const data = {
+  aviability: "в наличии",
+  price: "590 руб",
+  productName: "Номерки для игры мафия «Сицилия»",
+};
+const CardItem = () => {
+  const { aviability, price, productName } = data;
+  return (
+    <div className="card">
+      <div className="card-item">
+        <span>{aviability}</span>
+        <img
+          src="http://course.fudzi.kz/numerok.jpg"
+          alt={productName}
+          className="product-img"
+        />
+      </div>
+      <span>{productName}</span>
+      <div className="card-footer">
+        <span className="price">Цена: {price}</span>
+        <a href="https://google.com" className="btn">
+          Купить
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
