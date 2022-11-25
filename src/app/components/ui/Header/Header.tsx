bfa2a5f19b8bf9c02ac4c826e4d0e9935f49e2fc
Volodia <volodiasyd@gmail.com>
2022-11-25 21:29:55 +0200

    added slogan

--- src/app/components/ui/Header/Header.tsx
+++ src/app/components/ui/Header/Header.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import styles from './Header.module.scss';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
+   const mainText = 'Тут должен быть слоган';
+
    return (
       <div className={styles.header}>
          <div className={styles.head}>
             <div className={styles.container}>
-               <div className={styles.image}></div>
+               <div className={styles.main_img}>
+                  <div className={styles.img_container}>
+                     <Link to="/">
+                        <div className={styles.image}></div>
+                     </Link>
+                     <p>{mainText}</p>
+                  </div>
+               </div>
+
                <div className={styles.input}>
                   <input type="text" placeholder="Поиск товаров" />
                </div>
-               <div className={styles.number}>
-                  <p>8 (064)765-23-34</p>
-                  <button>
-                     <img
-                        src="https://icon-library.com/images/icon-arrow-down/icon-arrow-down-1.jpg"
-                        alt="arrow-down"
-                     />
-                  </button>
-               </div>
-               <div className={styles.line}></div>
-               <div className={styles.registration}>
-                  <button>
-                     Вход / Регистрация
-                     <img
-                        src="https://icon-library.com/images/icon-arrow-down/icon-arrow-down-1.jpg"
-                        alt="arrow-down"
-                     />
-                  </button>
+               <div className={styles.drop_down}>
+                  <div className={styles.number}>
+                     <p>
+                        <span>8</span> (064)765-23-34
+                     </p>
+                     <button></button>
+                  </div>
+                  <div className={styles.line}></div>
+                  <div className={styles.registration}>
+                     <button>Вход / Регистрация</button>
+                  </div>
                </div>
             </div>
          </div>
