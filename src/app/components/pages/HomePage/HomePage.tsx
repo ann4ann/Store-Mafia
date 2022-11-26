ae884e4853515436722f0bcf96150b3aa3b447e9
Sallein <saleinkyl@gmail.com>
2022-11-26 07:36:38 +0300

    remove change in HomePage

--- src/app/components/pages/HomePage/HomePage.tsx
+++ src/app/components/pages/HomePage/HomePage.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
-import { CardDetailGalery } from './../../ui/CardDetailGalery/CardDetailGalery';
 
 const HomePage = () => {
-  return <div className={styles.homePage}><CardDetailGalery/></div>;
+  return <div className={styles.homePage}>HomePage</div>;
 };
 
 export default HomePage;
