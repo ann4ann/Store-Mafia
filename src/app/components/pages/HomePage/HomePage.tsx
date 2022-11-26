009e5a80fa7806f31692bcae88d92ae91bff4fa9
Sallein <saleinkyl@gmail.com>
2022-11-26 10:19:22 +0300

    CardDetailDescription bottom part

--- src/app/components/pages/HomePage/HomePage.tsx
+++ src/app/components/pages/HomePage/HomePage.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
-import { CardDetailDesc } from './../../ui/CardDetailDesc/CardDetailDesc';
 
 const HomePage = () => {
-  return <div className={styles.homePage}><CardDetailDesc /></div>;
+  return <div className={styles.homePage}>HomePage</div>;
 };
 
 export default HomePage;
