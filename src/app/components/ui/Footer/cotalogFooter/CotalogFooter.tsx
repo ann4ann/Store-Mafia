b7447b848ca5070d07cbd5e436d9945a8d07dfe2
ilya <ilya123558ilya@gmail.com>
2022-12-11 00:13:32 +0300

    add interface

--- src/app/components/ui/Footer/cotalogFooter/CotalogFooter.tsx
+++ src/app/components/ui/Footer/cotalogFooter/CotalogFooter.tsx
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom';
 import styles from './CotalogFooter.module.scss'
 
+interface item {
+    title: string,
+    link: string
+}
+
 const CotalogFooter: React.FC = () => {
 
-    const items: { title: string, link: string }[] = [
-        { title: 'Набор для мафии', link: '#' },
-        { title: 'Маски', link: '#' },
-        { title: 'Номерки', link: '#' },
-        { title: 'Карты', link: '#' },
-        { title: 'Скатерти', link: '#' },
-        { title: 'Таблички', link: '#' },
-        { title: 'Сувениры', link: '#' },
+    const items: item[] = [
+        { title: 'Наборы', link: '/sets' },
+        { title: 'Маски', link: '/masks' },
+        { title: 'Номерки', link: '/numbers' },
+        { title: 'Карты', link: '/cards' },
+        { title: 'Скатерти', link: '/tablecloths' },
+        { title: 'Таблички', link: '/tablets' },
+        { title: 'Сувениры', link: '/souvenirs' },
     ]
-
+    
     return (
         <div className={styles.cotalogFooter}>
             <h3>Каталог</h3>
